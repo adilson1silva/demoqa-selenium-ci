fa98fc9fc65c569dbbb5c1833f7d6ed282871785
Adisilva1 <adilsons265@hotmail.com>
2025-11-11 07:49:33 +0000

    Ignore arquivos de configuracao de IntelliJ (.idea)

--- src/test/java/basetest/BaseTest.java
+++ src/test/java/basetest/BaseTest.java
@@ -2,6 +2,7 @@ package basetest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -9,16 +10,25 @@ public class BaseTest {
     protected WebDriver driver;
 
     @BeforeClass
-    public void beforeClass(){
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    public void beforeClass() {
+        ChromeOptions options = new ChromeOptions();
+
+        // Configurações essenciais para rodar no background (headless)
+        options.addArguments("--headless=new"); // Usa o novo modo headless do Chrome
+        options.addArguments("--no-sandbox");   // Necessário para rodar no GitHub Actions
+        options.addArguments("--disable-dev-shm-usage"); // Evita erros de memória compartilhada
+        options.addArguments("--disable-gpu");  // Desativa GPU (melhor compatibilidade)
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
         driver.get("https://demoqa.com/elements");
     }
 
     @AfterClass
-    public void afterClass(){
-        if(driver != null){
+    public void afterClass() {
+        if (driver != null) {
             driver.quit();
         }
     }
 }
+
