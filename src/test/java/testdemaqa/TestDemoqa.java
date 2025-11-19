0d14c85c1d0ab4ec4019bad92d30643ee19939d4
Adisilva1 <adilsons265@hotmail.com>
2025-11-19 19:53:47 +0000

    escrever um codigo mais limpo

--- src/test/java/testdemaqa/TestDemoqa.java
+++ src/test/java/testdemaqa/TestDemoqa.java
@@ -5,117 +5,101 @@ import org.example.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utilsclass.UtilsClass;
 
-import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
 
 public class TestDemoqa extends BaseTest {
 
-    UtilsClass utilsClass = new UtilsClass(driver);
-    ElementsPage elements = new ElementsPage(driver);
-    TextBox textbox = new TextBox(driver);
-    WebTabela  webTabela = new WebTabela(driver);
+    private UtilsClass utils;
+    private ElementsPage elements;
+    private TextBox textBox;
+    private WebTabela webTabela;
+    private ButtonsPage buttonsPage;
+    private BrokeLink brokeLink;
 
+    @BeforeMethod
+    public void setupPages() {
+        utils = new UtilsClass(driver);
+        elements = new ElementsPage(driver);
+        textBox = new TextBox(driver);
+        webTabela = new WebTabela(driver);
+        buttonsPage = new ButtonsPage(driver);
+        brokeLink = new BrokeLink(driver);
 
+        driver.get("https://demoqa.com/elements");
+    }
 
     @Test
-    public void devepreencherTextoFormularioComSucesso() throws InterruptedException {
-        driver.get("http://demoqa.com/elements");
-
-        //aqui criei uma funcao para abrir o Text Box
-        ElementsPage elements = new ElementsPage(driver);
+    public void devePreencherTextoFormularioComSucesso() {
         elements.Open_textBox();
 
-        // vou criar mais uma funcao para para preencher os dados
-        TextBox textbox = new TextBox(driver);
-        textbox.preencherFormulario("Adilson Mendes Silva", "teste@gmail.com", "rua cardoso tavares");
+        textBox.preencherFormulario(
+                "Adilson Mendes Silva",
+                "teste@gmail.com",
+                "rua cardoso tavares"
+        );
 
         WebElement submit = driver.findElement(By.id("submit"));
-        utilsClass.ScrollFindElement(driver, submit);
+        utils.ScrollFindElement(driver, submit);
         submit.click();
 
-
-        String resultado = textbox.obterNomoMostrado();
-        Assert.assertEquals(resultado, "Name:Adilson Mendes Silva", "Nome nao foi encontrado.");
-
-
+        Assert.assertEquals(
+                textBox.obterNomoMostrado(),
+                "Name:Adilson Mendes Silva",
+                "Nome não foi encontrado."
+        );
     }
 
     @Test
-    public void deveAbrirRadioButtonComSucesso() throws InterruptedException {
-        driver.get("https://demoqa.com/elements");
-
-        //criar uma funcao
-        ElementsPage elements = new ElementsPage(driver);
+    public void deveAbrirRadioButtonComSucesso() {
         elements.OpenRadioButton();
+        // Verifique algo real aqui futuramente
     }
 
-
-
-    /*
-    * web tabela acoes a realizar
-    *  - adicionar dados tabela
-    *  - remover dados na tabela
-    *  - editar dados na tabela
-    * */
     @Test
-    public void TrabalharWebTabela() throws InterruptedException {
-        driver.get("http://demoqa.com/elements");
-
-        WebElement teste = driver.findElement(By.xpath("//span[@class=\"text\" and text()=\"Web Tables\"]"));
-        utilsClass.ScrollFindElement(driver, teste);
-        ElementsPage elements = new ElementsPage(driver);
+    public void trabalharComWebTabela() throws InterruptedException {
         elements.OpenWebTable();
 
-        //adicionar dados na tabela
-        WebTabela  webTabela = new WebTabela(driver);
-        webTabela.addicionarDadosTabela("Adilson", "Silva", "adilsons265@gmail.com", "28", "70000", "test", "");
+        // Adicionar dados
+        webTabela.addicionarDadosTabela(
+                "Adilson", "Silva", "adilsons265@gmail.com",
+                "28", "70000", "test", ""
+        );
 
-        //editar dados na tabela
-        webTabela.editardados("Cierra", "Ciirra", "Teixeira", "","21", "7000", "TI", "");
+        // Editar dados
+        webTabela.editardados(
+                "Cierra", "Ciirra", "Teixeira",
+                "", "21", "7000", "TI", ""
+        );
 
-        //eliminar dados
+        // Remover dados
         webTabela.eliminardados("Alden", "Delete");
-        Thread.sleep(2000);
-
     }
 
     @Test
-    public void testDoubleClickButton() throws InterruptedException {
-
-        ElementsPage elements = new ElementsPage(driver);
+    public void testDoubleClickButton() {
         elements.OpenButtons();
 
-        //fazer dublo click
-        ButtonsPage buttonsPage = new ButtonsPage(driver);
+        // Duplo clique
         buttonsPage.DoDubleClick();
-
         assertTrue(buttonsPage.isDoubleClickMessageDisplayed());
-        //assertEquals(buttonsPage.getDoubleClickMessageText(), "You have done a right click");
 
-        //fazer click direito
+        // Clique direito
         buttonsPage.DoRightClick();
         assertTrue(buttonsPage.isDoubleClickMessageDisplayed());
-        //assertEquals(buttonsPage.getDoubleClickMessageText(), "You have done a right click");
 
-        //click
+        // Clique normal
         buttonsPage.DoClickMe();
         assertTrue(buttonsPage.isDoubleClickMessageDisplayed());
-        //assertEquals(buttonsPage.getDoubleClickMessageText(), "You have done a dynamic click");
     }
-
 
     @Test
-    public void testBrokeLink() throws InterruptedException {
-        ElementsPage elements = new ElementsPage(driver);
+    public void testBrokenLink() {
         elements.OpenBrokenLinks_images();
-
-        BrokeLink  brokeLink = new BrokeLink(driver);
         brokeLink.brokeLinks();
-
     }
-
 
 }
